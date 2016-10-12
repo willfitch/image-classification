80dd8683ae20228fa2e40867e3e46d498f708a75
Will Fitch <wfitch@meetme.com>
2016-10-12 15:21:48 -0400

    Update package info and comments

--- server.js
+++ server.js
@@ -9,27 +9,6 @@ var controllerMap = {
   queue: new Queue()
 };
 
-/*
-server.get('/hi', function(req, res, next) {
-  var obj = {
-    name: "Will",
-    email: "wfitch@meetme.com",
-    uuid: uuid.v4()
-  };
-  log.info('Ok...');
-
-  var img = new ImageProcessing();
-  img.topicName = "test.me";
-  img.processUuid = uuid.v4();
-  img.remoteAddr = req.connection.remoteAddress
-  img.save();
-
-  console.log(img.remoteAddr);
-
-  res.send(obj);
-  next();
-});*/
-
 // Log the request
 server.use(function (req, res, next) {
   log.info('[client ' + req.connection.remoteAddress + ']' + ' "' + req.method + ' ' + req.url + '"');
